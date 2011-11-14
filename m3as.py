cdd23f462564f496c8c81308e9b5acfc772dc2bf
Rodrigo Parra <rodpar07@gmail.com>
2011-11-14 19:41:50 -0300

    Agregado módulo base MOACO y módulo M3AS

---
+++ m3as.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class TspM3AS(Moaco):
+	def __init__(self, taumax, taumin, beta, cost_mats, total_ants):
+		super(TspM3AS, self).__init__(taumax, taumin, beta, cost_mats, total_ants)
+		
+
+
+
+def main():
+	taumax = 0.0023
+	taumin = 0.000071
+	beta = 1
+	return 0
+	return 0
+
+if __name__ == '__main__':
+	main()
+
---
+++ moaco.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from parser import *
+
+class Moaco():
+	
+	def __init__(self, taumax, taumin, beta, cost_mats, total_ants):
+		self.taumax = taumax
+		self.taumin = taumin
+		self.beta = beta
+		self.total_ants = total_ants
+		self.visib_mats = []
+		self.pareto_set = []
+		for cost_mat in xrange(len(cost_mats)):
+			visib_mat = []
+			for row in xrange(len(cost_mat)):
+					visib_mat.append([1.0/e for e in row])
+			self.visib_mats.append(visib_mat)
+		
+	def run():
+		raise NotImplementedError("evaluate method has to be implemented.")
+
+	
+	
+	
