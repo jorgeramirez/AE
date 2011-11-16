885cf329d3dcc3e059ada49513dfb5b8e1050970
caronig <gabrielcaroni@gmail.com>
2011-11-16 17:50:21 -0300

    Test TSP

---
+++ testtsp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import m3as
+import moacs
+import sys
+from solution import *
+
+def main():
+    # Recibe por terminal el nro. de instancia a resolver.
+    instance = int(sys.argv[1]) - 1
+    pareto_set_true = ParetoSet(None)
+    pareto_set_m3as = m3as.testTsp(i = instance)
+    pareto_set_true.update(pareto_set_m3as.solutions)
+    pareto_set_moacs = moacs.testTsp(i = instance)
+    pareto_set_true.update(pareto_set_moacs.solutions)
+    return 0
+
+if __name__ == '__main__':
+    main()
