33cd08acf8dbe355281d2087dd4a17024dec7872
Rodrigo Parra <rodpar07@gmail.com>
2011-11-16 12:29:46 -0300

    Agregado módulo metric.

--- testqap.py
+++ testqap.py
@@ -8,8 +8,6 @@ from solution import *
 def main():
     # Recibe por terminal el nro. de instancia a resolver.
     instance = int(sys.argv[1]) - 1
-    print instance
-    print type(instance)
     pareto_set_true = ParetoSet(None)
     pareto_set_m3as = m3as.testQap(i = instance)
     pareto_set_true.update(pareto_set_m3as.solutions)
